8ca248368613f0f685058ded59d6475e3b90cbe8
dhairya10p22 <dhairyasharmav369@gmail.com>
2026-01-09 03:28:18 +0530

    d

--- january3/src/app/app.ts
+++ january3/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 
@@ -14,10 +14,35 @@ export class App {
    num:number=22;
    data:number|string='22';
    obj:any;
+   email=" ";
    count:number=0;
    increment (){
     this.count+=1;
    }
+
+   handleinputEvent(event:Event){
+    this.name=(event.target as HTMLInputElement).value;
+   }
+   handlemouseoverEvent(event:Event){
+    console.log('hi');
+
+
+   }
+   getEmail(val:String){
+    this.email=val;
+
+   }
+   setEmail(email:string){
+    this.email=email;
+
+   }
+   handlekeyupEvent(event:Event){
+
+        console.log('add 2 nume');
+
+
+   }
+
    decrement (){
     if(this.count>0){
     this.count=this.count-1;}
